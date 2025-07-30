4e3cb20186c767c37f70a79eb4f4ac06e1ceb09c
alfrye <alfrye@gmail.com>
2025-07-29 17:54:32 -0700

    Added vim options for linenumber, search highlight and cursor line

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -2,5 +2,9 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set cursorline")
+vim.cmd("set hlsearch")
 vim.g.mapleader = " "
 
