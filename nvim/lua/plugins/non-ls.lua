18afbf4e91ec7061589ba5e1e7b79b58b3d3b969
alfrye <alfrye@gmail.com>
2025-08-05 22:28:53 -0700

    updated non-ls config to include diagnostic for bashls

--- nvim/lua/plugins/non-ls.lua
+++ nvim/lua/plugins/non-ls.lua
@@ -5,8 +5,9 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.shfmt,
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+  end
 }
