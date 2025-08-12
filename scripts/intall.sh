a495d57857867f57229a2c4d5cca9c82ff497923
alfrye <alfrye@gmail.com>
2025-08-11 21:39:55 -0700

    added directory check for .config to the intall.sh"

--- scripts/intall.sh
+++ scripts/intall.sh
@@ -4,5 +4,12 @@
 set -e
 
 echo "instaling dotfiles"
+DIRECTORY="/home/vscode/.config"
 
+if [ -d "$DIRECTORY" ]; then
+  echo "Directory '$DIRECTORY' exists."
+else
+  echo "Directory '$DIRECTORY' does not exist."
+  mkdir -p "$DIRECTORY"
+fi
 ln -s ~/dotfiles/nvim  ~/.config/
