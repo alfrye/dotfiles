a27a8157a60b66108361f4c6556c9526650e7e48
alfrye <alfrye@gmail.com>
2025-10-18 12:22:35 -0700

    enabled language servers for go

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -17,11 +17,14 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
        local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
-         capabilites = capabilities
-      })
-      lspconfig.bashls.setup({})
+      -- local lspconfig = require("lspconfig")
+      -- lspconfig.lua_ls.setup({
+      --    capabilites = capabilities
+      -- })
+      -- lspconfig.bashls.setup({})
+      vim.lsp.enable("bashls")
+      vim.lsp.enable("lua_ls")
+      vim.lsp.enable("gopls")
        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({'n', 'v'}, '<leader>ca',  vim.lsp.buf.code_action,{})
