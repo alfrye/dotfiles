371d401f57793d4138a35d35893851499e1a3cbb
alfrye <alfrye@gmail.com>
2025-10-18 08:52:39 -0700

    init.lua fixed typo in url for cloning lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,7 +10,7 @@ if not vim.loop.fs_stat(lazypath) then
 		"git",
 		"clone",
 		"--filter=blob:none",
-    "https://github.xom/folke/lazy.nvim.git",
+    "https://github.com/folke/lazy.nvim.git",
 		"--branch=stable",
 		lazypath,
 	})
