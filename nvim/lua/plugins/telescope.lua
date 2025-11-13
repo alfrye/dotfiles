8298b146ceb80a37029f83e4ff3f7d5c0c647146
alfrye <alfrye@gmail.com>
2025-11-12 22:35:30 -0800

    Added list buffers keymaps to telescope configuration

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
       builtin = require("telescope.builtin")
       vim.keymap.set('n', '<leader>ff', builtin.find_files,{})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep,{})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc= 'List Buffers'})
     end
    },
    {
