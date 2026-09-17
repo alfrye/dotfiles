42658758e187d32b473f96a2c9183d85905d39d2
alfrye <alfrye@gmail.com>
2026-09-16 19:27:59 -0700

    added oil.nvim to configuration updated non-ls to include go formatter

--- nvim/lua/plugins/non-ls.lua
+++ nvim/lua/plugins/non-ls.lua
@@ -6,8 +6,16 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.shfmt,
+        null_ls.builtins.formatting.gofmt,
+        null_ls.builtins.formatting.goimports,
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = { "*.go" },
+      callback = function()
+        vim.lsp.buf.format({ timeout_ms = 1000 })
+      end,
+    })
   end
 }
